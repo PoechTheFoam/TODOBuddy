d404f82ec9151d758bd082a7bb5f7404d558fe29
Poech <nhmp96@gmail.com>
2025-09-14 12:04:36 +0700

    Delete images/src/main directory

--- images/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example.smarttodolist {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.example.smarttodolist to javafx.fxml;
-    exports org.example.smarttodolist;
-}
